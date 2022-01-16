29f325e98521eece600d87cdb9ecc63e2a23cd95
Gašper Katrašnik <72498702+gkatrasnik@users.noreply.github.com>
2022-01-16 09:07:39 +0100

    Update Aboutme.js

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -18,7 +18,7 @@ const Aboutme = () => {
           Mechatronics and learned programming. I first met programming when I
           was 14 years old, when I created my first "hello world" in Visual
           Basic. In the years that followed, I was on and off with programming,
-          until 2021 when the covid crisis hit. I had all the time in the world,
+          until 2020 when the covid crisis hit. I had all the time in the world,
           so I decided to commit and learn programming for real. A friend of
           mine suggested me "Odin Project", open source web developement
           curriculum and it took me about a year to complete it. In this time I
